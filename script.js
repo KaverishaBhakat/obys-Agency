6cbe46f1dd81ae802188e1da83a9757d641c6894
Kaverisha Bhakat <mistum160303@gmail.com>
2025-12-18 17:30:42 +0530

    Made loader successfully

---
+++ script.js
@@ -0,0 +1,56 @@
+var tl = gsap.timeline()
+// the upcoming of text
+tl.from(".line h1", {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5,
+})
+// upcoming of now
+tl.from(".line h2", {
+    y: 150,
+    duration: 0.5,
+}, "-=0.55")
+
+// features of countdown and now
+tl.from("#line1-part1", {
+    opacity: 0,
+    onStart: function () {
+        var h5timer = document.querySelector("#line1-part1 h5")
+        var grow = 0
+        setInterval(function () {
+            if (grow < 100) {
+                h5timer.innerHTML = grow++
+            }
+            else {
+                h5timer.innerHTML = grow
+            }
+
+            console.log(grow)
+        }, 20)
+    }
+}, "-=0.5");
+
+// invisible loader
+tl.to("#loader", {
+    opacity: 0,
+    duration: 0.8,
+    delay: 3
+})
+// upcoming of page1
+tl.from("#page1", {
+    y: 1200,
+    opacity: 0,
+    duration: 1.7,
+    ease: "expo.out"
+})
+// visualizing the note
+tl.from(".note h5", {
+    y: 0,
+    opacity: 0,
+    duration: 0.6
+}, 1.4);
+// after page1 loaded the loader display is not in the bg
+tl.to("#loader", {
+    display: none,
+})
