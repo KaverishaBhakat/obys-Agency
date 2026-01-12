7bfd27d8676026bc160220ad52d09c6e08272a56
Kaverisha Bhakat <mistum160303@gmail.com>
2026-01-12 22:42:29 +0530

    upgraded code

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function locomotive() {
   gsap.registerPlugin(ScrollTrigger);
 
+
 // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
-  smooth: true
+  smooth: true,
+//    lerp: 0.07, // Add this for smoothness!
+//         multiplier: 1
 });
 // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
 locoScroll.on("scroll", ScrollTrigger.update);
